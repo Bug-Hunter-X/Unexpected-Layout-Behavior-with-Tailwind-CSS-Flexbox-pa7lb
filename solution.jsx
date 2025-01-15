5891c89474415b8b70e44b2cd98bbf2fb8530047
BugHunter-X <askzenai@gmail.com>
2025-01-15 06:50:05 +0100

    Add solution.jsx file

---
+++ solution.jsx
@@ -0,0 +1,9 @@
+```javascript
+// solution.jsx
+<div className="bg-gray-200 p-4 h-24">
+  <div className="flex">
+    <div className="w-1/2 bg-red-500 p-2">Div 1</div>
+    <div className="w-1/2 bg-blue-500 p-2">Div 2</div>
+  </div>
+</div>
+```
